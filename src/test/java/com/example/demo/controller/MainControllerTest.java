635ecc62239eeb129b31fc4e3e5ef1fa264126a8
kimjunhyeong <cosyq1305@naver.com>
2025-03-20 22:34:12 +0900

    mybatis 환경 테스트...

--- src/test/java/com/example/demo/controller/MainControllerTest.java
+++ src/test/java/com/example/demo/controller/MainControllerTest.java
@@ -1,30 +1,32 @@
 package com.example.demo.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+//@SpringBootTest
+@WebMvcTest(MainController.class)
+@AutoConfigureMybatis
+@ActiveProfiles("dev")
 class MainControllerTest {
-
-    @Autowired
-    MainController mainController;
 
     @Autowired
     private MockMvc mockMvc;
 
 
     @Test
+    @DisplayName("Hello, World! 출력 테스트")
     public void hello() throws Exception {
-            mockMvc.perform(
-                    MockMvcRequestBuilders.get("/"))
+            mockMvc.perform(get("/"))
                     .andExpect(status().isOk())
                     .andExpect(content().string("Hello, World!"));
     }
