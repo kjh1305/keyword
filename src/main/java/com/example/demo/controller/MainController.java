92f4950bf20cc94cd603789cd4a640cbfc84451d
kimjunhyeong <cosyq1305@naver.com>
2025-03-29 02:03:54 +0900

    포트폴리오 추가해보기

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping
+    @ResponseBody
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/portfolio")
+    public String portfolio() {
+        return "portfolio/portfolio.html";
     }
 }
