32ba0b59e06a92c05b73d989679b7a9d469dda1a
kimjunhyeong <cosyq1305@naver.com>
2026-02-18 01:35:36 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan(basePackages = {"com.example.demo.api.keyword.work", "com.example.demo.api.keyword.backup",
 "com.example.demo.api.keyword.category", "com.example.demo.api.keyword.rank"})
 @EnableJpaRepositories
